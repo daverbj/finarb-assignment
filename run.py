4a774cbc1a2ecc51f418c0ef696a5c96a7d15833
daveai <dave@agileinf.com>
2019-05-29 19:38:56 +0530

    adding config

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os 
+import config
 def create_app(config_filename):
     app = Flask(__name__)
-    '''app.config.from_object(config_filename)
-    
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    """ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True """
+    app.config.from_object(config.DevelopmentConfig)
+    db = SQLAlchemy(app)
+    ma = Marshmallow(app)
     from app import api_bp
     app.register_blueprint(api_bp, url_prefix='/api')
-
-    from Model import db
-    db.init_app(app)'''
-    from app import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-
     return app
-
 
 if __name__ == "__main__":
     app = create_app("config")
